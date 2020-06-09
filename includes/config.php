a94fbc6dc00d0e4c9776cc17798194bd1203dd81
Jim Wang <jimcwx@gmail.com>
2020-06-09 15:20:20 -0400

    hooking up signup to database

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php 
+  ob_start();
+  $timezone = date_default_timezone_set("America/Toronto");
+  $con = mysqli_connect("localhost", "root", "", "spotifyclone");
+
+  if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+  }
+?>
--- register.php
+++ register.php
@@ -1,9 +1,17 @@
 <?php 
+
+  include("includes/config.php");
   include("includes/classes/Account.php");
   include("includes/classes/Constants.php");
-  $account = new Account();
+  $account = new Account($con);
   include("includes/handlers/register-handler.php");
   include("includes/handlers/login-handler.php");
+
+  function getInputValue($name) {
+    if (isset($_POST[$name])) {
+      echo $_POST[$name];
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -35,27 +43,27 @@
       <p>
         <?php echo $account->getError(Constants::$usernameCharacters); ?>
         <label for="username">Username</label>
-        <input type="text" id="username" name="username" placeholder="e.g. bartSimpson" required>
+        <input type="text" id="username" name="username" placeholder="e.g. bartSimpson" value="<?php getInputValue("username") ?>" required>
       </p>
       <p>
         <?php echo $account->getError(Constants::$firstNameCharacters); ?>
         <label for="firstName">First Name</label>
-        <input type="text" id="firstName" name="firstName" placeholder="e.g. Bart" required>
+        <input type="text" id="firstName" name="firstName" placeholder="e.g. Bart" value="<?php getInputValue("firstName") ?>" required>
       </p>
       <p>
         <?php echo $account->getError(Constants::$lastNameCharacters); ?>
         <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName" placeholder="e.g. Simpson" required>
+        <input type="text" id="lastName" name="lastName" placeholder="e.g. Simpson" value="<?php getInputValue("lastName") ?>" required>
       </p>
       <p>
         <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
         <?php echo $account->getError(Constants::$emailInvalid); ?>
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="e.g. bart@gmail.com" required>
+        <input type="email" id="email" name="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue("email") ?>" required>
       </p>
       <p>
         <label for="email2">Confirm Email</label>
-        <input type="email" id="email2" name="email2" placeholder="e.g. bart@gmail.com" required>
+        <input type="email" id="email2" name="email2" placeholder="e.g. bart@gmail.com" value="<?php getInputValue("email2") ?>" required>
       </p>
 
       <p>
