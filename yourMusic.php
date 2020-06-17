175cc9da73993594abd3498896be6976bbb3826c
Jim Wang <jimcwx@gmail.com>
2020-06-17 15:37:58 -0400

    add playlists

--- yourMusic.php
+++ yourMusic.php
@@ -5,6 +5,32 @@
 <div class="playlistContainer gridViewContainer">
   <h2>PLAYLISTS</h2>
   <div class="buttonItems">
-    <button class="button green">NEW PLAYLIST</button>
+    <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
   </div>
+
+  <?php
+    $username = $userLoggedIn->getUsername();
+
+    $playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username' ");
+
+    if(mysqli_num_rows($playlistsQuery) == 0) {
+        echo "<span class='noResults'>No Playlists Yet</span>";
+      }
+
+    while($row = mysqli_fetch_array($playlistsQuery)) {
+      $playlist = new Playlist($con, $row);
+
+      echo 
+        "<div class='gridViewItem' role='link' tabindex='0' onclick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
+          <div class='playlistImage'>
+            <img src='assets/images/icons/playlist.png'>
+          </div>
+          <div class='gridViewInfo'>"
+            . $playlist->getName() .
+          "</div>
+        </div>";
+
+    }
+  ?>
+
 </div>
